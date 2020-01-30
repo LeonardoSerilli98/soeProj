b033f085cb610619e30e48c5c2c0c0e784da1ffe
jucola98 <gabriele.colapelle@gmail.com>
2020-01-30 15:46:03 +0100

    serchbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,8 @@
       CSS
       =============================================
     -->
+    
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="{{asset('css/linearicons.css')}}" />
   <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" />
   <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}" />
@@ -23,7 +25,7 @@
   <header class="default-header">
     <nav class="navbar navbar-expand-lg  navbar-light">
       <div class="container">
-        <a class="navbar-brand" href="index.html">
+        
         @if(Auth::check())                                       
             <a href="{{ route('logout') }}" 
                   onclick="event.preventDefault();
@@ -42,14 +44,21 @@
           <a href="{{route('auth')}}" > Login / SingUp </a>
           @endif
           
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="lnr lnr-menu"></span>
-        </button>
+        
+        
 
-        <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
-          <ul class="navbar-nav">
+        
+          
+            <div class="search">
+              <input type="text" class="searchTerm" placeholder="What are you looking for?">
+                <button type="submit" class="searchButton">
+                  <i class="fa fa-search"></i>
+                </button>
+            </div>
+          
+        
+          
+          
             <li><a href="{{route('master')}}">Home</a></li>
             
             @if(Auth::check())
@@ -64,38 +73,34 @@
             
             </li>
             <li><a href="contacts.html">Contacts</a></li>
-
-            <li>
-              <button class="search">
-                <span class="lnr lnr-magnifier" id="search"></span>
-              </button>
-            </li>
-          </ul>
-        </div>
+            
+          
+          
+        
       </div>
+      
     </nav>
-    <div class="search-input" id="search-input-box">
-      <div class="container">
-        <form class="d-flex justify-content-between">
-          <input type="text" class="form-control" id="search-input" placeholder="Search Here" />
-          <button type="submit" class="btn"></button>
-          <span class="lnr lnr-cross" id="close-search" title="Close Search"></span>
-        </form>
-      </div>
-    </div>
-  </header>
+    
+    </header>
+    
   <!-- ================ End Header Area ================= -->
   
   @yield("content");
 
-  <script src="{{asset('js/vendor/jquery-2.2.4.min.js')}}"></script>
+  <script src="js/vendor/jquery-2.2.4.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
     crossorigin="anonymous"></script>
-  <script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
+  <script src="js/vendor/bootstrap.min.js"></script>
   <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-  <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
-  <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
-  <script src="{{asset('js/main.js')}}"></script>
-</body>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/parallax.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/hexagons.min.js"></script>
+  <script src="js/jquery.counterup.min.js"></script>
+  <script src="js/waypoints.min.js"></script>
+  <script src="js/jquery.nice-select.min.js"></script>
+  <script src="js/main.js"></script>
 
 </html>
