ff8d11be149a3a7cf9e9507a2bbb9d14f8dd73f5
jucola98 <gabriele.colapelle@gmail.com>
2020-01-31 21:39:00 +0100

    si

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,21 @@
 
   <!-- ================ start banner Area ================= -->
 
-          <h1 class="text-white">
+          <h1 >
             Interfaccia prototipo SE
           </h1>
-          <p class="mx-auto text-white  mt-20 mb-40">
+          <p>
             gruppo: LAG; progetto Sharing di Appunti
           </p>
 
-        <p class="mx-auto text-white  mt-20 mb-40">
-            Utilizzo del materiale pubblicato
+        <ul class="info">
+            Utilizzo del materiale pubblicato<br>
 
             I documenti e le immagini presente in qualsiasi sezione del sito e in qualsivoglia formato possono essere scaricati solo per un uso personale e non commerciale: pertanto nulla, neppure in parte, potrà essere copiato, modificato, distribuito per fini di lucro.
             Eventuali utilizzi in ambito commerciale potranno avvenire, in conformità alla normativa vigente, previa richiesta di autorizzazione da inoltrare all’indirizzo redazionale riportato nella sezione Contatti.
             La pubblicazione su altri siti (ad eccezione di quelli a scopo commerciale, per i quali si applica il precedente capoverso) di detto materiale, anche solo in parte, è consentita esclusivamente se accompagnata dalla chiara citazione della fonte.
             Qualsiasi forma di link al presente sito, se inserita da soggetti terzi, deve essere effettuata in conformità alla normativa vigente e non deve recare danno all’immagine e all’attività del sito.
-        </p>
+        </ul>
 
 
 @endsection
